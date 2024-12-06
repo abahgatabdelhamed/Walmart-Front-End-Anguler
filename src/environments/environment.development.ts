3aeccbd16a872217c8066b802ce532792bbe3dfe
abahgatabdelhamed <ahmedbahgat1606@gmail.com>
2024-12-06 16:46:44 +0200

    aaa

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  baseURL:'https://localhost:7016/api',
-  baseuRL: 'https://localhost:7016/api',
-  url:"https://localhost:7028"
+  baseURL:'https://walapi.runasp.net/api',
+  baseuRL: 'https://walapi.runasp.net/api',
+  // url:"https://localhost:7028"
+  url:"https://walmartdashboard.runasp.net"
 };
